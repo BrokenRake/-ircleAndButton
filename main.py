85f7cacf8a4bf1dd09a0b4310a790bc0f6eb4954
Broken_Rake <wffpiter@gmail.com>
2021-11-27 19:15:39 +0300

    Git и желтые окружности

---
+++ main.py
@@ -0,0 +1,36 @@
+import sys
+import random
+from PyQt5 import uic
+from PyQt5.QtGui import QPainter, QColor
+from PyQt5.QtWidgets import QMainWindow, QApplication
+
+
+class Example(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('UI.ui', self)
+        self.do_paint = False
+        self.pushButton.clicked.connect(self.paint)
+
+    def paintEvent(self, event):
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            self.circle(qp)
+            qp.end()
+
+    def circle(self, qp):
+        a = random.randrange(50, 200)
+        qp.setBrush(QColor(255, 204, 0))
+        qp.drawEllipse(150, 100, a, a)
+
+    def paint(self):
+        self.do_paint = True
+        self.repaint()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    ex.show()
+    sys.exit(app.exec())
